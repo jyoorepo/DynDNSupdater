41f93b346f20e3aa870876fd6de4edac318b4675
Joseph Yoo <joseph@jyoo.co>
2021-11-28 20:49:10 -0800

    If and Else statement has issues running in sequence. This file will be kept as an example for myself but is no longer in service.

--- dynautorunner.py
+++ dynautorunner.py
@@ -1,10 +1,23 @@
-import dynupdater
 import urllib.request
-import dynupdater
+import schedule
+import time
 
+externalIp = urllib.request.urlopen(
+    'https://api.ipify.org').read().decode('utf8')
 
+print("this is your current IP", externalIp)
 
-externalIp = urllib.request.urlopen('https://api.ipify.org').read().decode('utf8')
+def job():
+    print(urllib.request.urlopen('https://api.ipify.org').read().decode('utf8'))
 
+refreshedIP = job()
 
-exec(open("dynupdater.py").read())
+schedule.every(5).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    if refreshedIP == externalIp:
+        exec(open("dynupdater.py").read())
+    else:  
+        schedule.run_pending()
